b25d4fd45811513772c45dd17d7c2f3b880a0544
Jeff <jeff@Jeffs-MacBook-Pro.local>
2021-04-07 10:57:11 -0600

    GET implemented

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,15 @@
 const express = require("express");
 const router = express.Router();
 
+function find_id(list, id){
+    for(let i = 0; i < list.length; i++){
+        if(list[i]._id == id){
+            return list[i];
+        }
+    }
+    return false;
+}
+
 router.route("/")
     .get((req, res) => {
         console.log("GET /");
@@ -23,7 +32,7 @@ const data = require("../config/data.json");
 router.route("/doctors")
     .get((req, res) => {
         console.log("GET /doctors");
-        res.status(501).send();
+        res.status(200).send(data.doctors);
     })
     .post((req, res) => {
         console.log("POST /doctors");
@@ -33,7 +42,13 @@ router.route("/doctors")
 router.route("/doctors/:id")
     .get((req, res) => {
         console.log(`GET /doctors/${req.params.id}`);
-        res.status(501).send();
+        let doctor = find_id(data.doctors, req.params.id);
+        if(doctor){
+            res.status(200).send(doctor);
+        }
+        else{
+            res.status(404).send();
+        }
     })
     .patch((req, res) => {
         console.log(`PATCH /doctors/${req.params.id}`);
@@ -47,7 +62,18 @@ router.route("/doctors/:id")
 router.route("/doctors/:id/companions")
     .get((req, res) => {
         console.log(`GET /doctors/${req.params.id}/companions`);
-        res.status(501).send();
+        let comps = [];
+        for(const companion of data.companions){
+            if(companion.doctors.includes(req.params.id)){
+                comps.push(companion);
+            }
+        }
+        if(comps.length == 0){
+            res.status(404).send();
+        }
+        else{
+            res.status(200).send(comps);
+        }
     });
 
 // router.route("/doctors/:id/companions/longest")
@@ -59,13 +85,24 @@ router.route("/doctors/:id/companions")
 router.route("/doctors/:id/goodparent")
     .get((req, res) => {
         console.log("GET /doctors/:id/goodparent");
-        res.status(501).send();
+        let comps = [];
+        for(const companion of data.companions){
+            if(companion.doctors.includes(req.params.id)){
+                comps.push(companion);
+            }
+        }
+        if(comps.length == 0){
+            res.status(404).send();
+        }
+        else{
+            res.status(200).send(comps.length == comps.filter(comp => comp.alive).length);
+        }
     });
 
 router.route("/companions")
     .get((req, res) => {
         console.log("GET /companions");
-        res.status(501).send();
+        res.status(200).send(data.companions);
     })
     .post((req, res) => {
         console.log("POST /companions");
@@ -73,15 +110,21 @@ router.route("/companions")
     });
 
 router.route("/companions/crossover")
-    .get((req, res) => {
-        console.log(`GET /companions/crossover`);
-        res.status(501).send();
-    });
+.get((req, res) => {
+    console.log(`GET /companions/crossover`);
+    res.status(200).send(data.companions.filter(companion => companion.doctors.length > 1));
+});
 
 router.route("/companions/:id")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}`);
-        res.status(501).send();
+        let comp = find_id(data.companions, req.params.id);
+        if(comp){
+            res.status(200).send(comp);
+        }
+        else{
+            res.status(404).send();
+        }
     })
     .patch((req, res) => {
         console.log(`PATCH /companions/${req.params.id}`);
@@ -95,14 +138,36 @@ router.route("/companions/:id")
 router.route("/companions/:id/doctors")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}/doctors`);
-        res.status(501).send();
+        let comp = find_id(data.companions, req.params.id);
+        if(comp){
+            res.status(200).send(data.doctors.filter(doctor => comp.doctors.includes(doctor._id)));
+        }
+        else{
+            res.status(404).send();
+        }
     });
 
 router.route("/companions/:id/friends")
     .get((req, res) => {
         console.log(`GET /companions/${req.params.id}/friends`);
-        res.status(501).send();
+        let comp = find_id(data.companions, req.params.id);
+        if(comp){
+            let friends = data.companions.filter(companion => {
+                for(let i = 0; i < comp.seasons.length; i++){
+                    for(let j = 0; j < companion.seasons.length; j++){
+                        if(comp.seasons[i] == companion.seasons[j] && comp._id != companion._id)
+                            return true;
+                    }
+                }
+                return false;
+            });
+            res.status(200).send(friends);
+        }
+        else{
+            res.status(404).send();
+        }
     });
+
 
 //////////////////
 // EXTRA CREDIT //
